9c4d5b5a77c9b083ffaf9e71cf411c587e227848
dbingabire05@gmail.com <dbingabire05@gmail.com>
2025-09-12 12:37:30 +0200

    removed files for exercise3 branch

--- exercise1.js
+++
@@ -1,7 +0,0 @@
-function nestedArray(num){
-if(!Array.isArray(num)) return false;
-return (num.some(element=>Array.isArray(element)))? num.flat(Infinity):false;
-
-}
-
-console.log(nestedArray([1,2,[3,4,[5,6]]]))
--- exercise3.js
+++
@@ -1,4 +0,0 @@
-function sumArray(num){
-    if (num.length===0) return 0;
-    return  num[0] + sumOfElements(num.slice(1))
-}
